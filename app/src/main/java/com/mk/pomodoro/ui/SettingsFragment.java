f3df1046cad83420b8eb692d80bfb2265ddf9f1a
Dario <106852390+Minkaspr@users.noreply.github.com>
2023-09-23 22:18:20 -0500

    pomodoro type

--- app/src/main/java/com/mk/pomodoro/ui/SettingsFragment.java
+++ app/src/main/java/com/mk/pomodoro/ui/SettingsFragment.java
@@ -9,17 +9,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mk.pomodoro.R;
+
 public class SettingsFragment extends Fragment {
 
     private LinearLayoutCompat llcTema;
+    private ConstraintLayout clClasico, clExtendido, clCorto, clPersonalizado;
+    private ImageView ivClasico, ivExtendido, ivCorto, ivPersonalizado;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,9 +41,44 @@ public class SettingsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         llcTema = view.findViewById(R.id.llTheme);
-
         llcTema.setOnClickListener(v -> mostrarDialogoTema(v));
+
+        ivClasico = view.findViewById(R.id.ivClassic);
+        ivExtendido = view.findViewById(R.id.ivExtended);
+        ivCorto = view.findViewById(R.id.ivShort);
+        ivPersonalizado = view.findViewById(R.id.ivPersonalized);
+
+        clClasico = view.findViewById(R.id.clClassic);
+        clExtendido = view.findViewById(R.id.clExtended);
+        clCorto = view.findViewById(R.id.clShort);
+        clPersonalizado = view.findViewById(R.id.clPersonalized);
+
+
+        clClasico.setOnClickListener(v -> activarVisibilidad(View.VISIBLE, View.INVISIBLE, View.INVISIBLE, View.INVISIBLE));
+        clExtendido.setOnClickListener(v -> activarVisibilidad(View.INVISIBLE, View.VISIBLE, View.INVISIBLE, View.INVISIBLE));
+        clCorto.setOnClickListener(v -> activarVisibilidad(View.INVISIBLE, View.INVISIBLE, View.VISIBLE, View.INVISIBLE));
+        clPersonalizado.setOnClickListener(v -> activarVisibilidad(View.INVISIBLE, View.INVISIBLE, View.INVISIBLE, View.VISIBLE));
     }
+
+    private void activarVisibilidad(int visibilityClasico, int visibilityExtendido, int visibilityCorto, int visibilityPersonalizado) {
+        animarVisibilidad(ivClasico, visibilityClasico);
+        animarVisibilidad(ivExtendido, visibilityExtendido);
+        animarVisibilidad(ivCorto, visibilityCorto);
+        animarVisibilidad(ivPersonalizado, visibilityPersonalizado);
+    }
+
+    private void animarVisibilidad(ImageView imageView, int visibility) {
+        if (visibility == View.VISIBLE && imageView.getVisibility() != View.VISIBLE) {
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setAlpha(0f);
+            imageView.animate().alpha(1f).setDuration(750);
+        } else if (visibility != View.VISIBLE) {
+            imageView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+
 
     private void mostrarDialogoTema(View v) {
         String[] temas = {"Sistema", "Claro", "Oscuro"};
